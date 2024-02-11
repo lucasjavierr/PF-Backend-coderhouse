e77136f25ad9aa520ee746c29f1558f26ff39533
Lucas Javier <santilujavier@gmail.com>
2024-02-11 05:07:03 -0300

    cambios en las variables de entorno

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,6 @@
 import dotenv from 'dotenv'
-// import { Command } from 'commander'
 
 dotenv.config()
-
-/* const serve = new Command()
-
-serve
-  .option( '-p, --persistence <persistence>', 'Tipo de persistencia de datos que utiliza la aplicación' )
-  .option( '--env, <environment>', 'Entorno en el cual se ejecuta la aplicación' )
-
-serve.parse()
-const options = serve.opts()
-process.env.NODE_ENVIRONMENT = options.env
-process.env.PERSISTENCE = options.persistence */
 
 export const config = {
   server: {
@@ -22,12 +10,11 @@ export const config = {
   },
   mongo: {
     url: process.env.MONGO_URL,
-    urlTest: process.env.MONGO_URL_TEST
   },
   gmail: {
-    account: process.env.ADMIN_EMAIL,
-    password: process.env.ADMIN_EMAIL_PASS,
-    secretToken: process.env.TOKEN_EMAIL
+    account: process.env.ADMIN_GMAIL,
+    password: process.env.ADMIN_GMAIL_PASS,
+    secretToken: process.env.TOKEN_GMAIL
   },
   admin: {
     id: process.env.ADMIN_ID,
